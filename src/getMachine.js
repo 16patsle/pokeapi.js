15cd6a5a1e49c77bfd83ed0ce45133a1077188fa
Patrick Sletvold <patricksletvold@hotmail.com>
2018-02-21 14:25:59 +0100

    Fix error in getMachine.js

--- src/getMachine.js
+++ src/getMachine.js
@@ -1,6 +1,6 @@
 var PokeApi = PokeApi || {};
 
-PokeApi.getMachine = function getItem(id) {
+PokeApi.getMachine = function getMachine(id) {
     return new Promise(function(resolve, reject) {
         PokeApi.getResource("machine/" + id).then(function(response) {
             resolve(response);
